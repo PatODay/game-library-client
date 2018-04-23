78478f5a98e42abc10cc1b4b72b3183aa3ac07d9
PatODay <poday22@gmail.com>
2018-04-23 18:01:05 -0400

    Added HTML forms & Auth Event actions
    Added sign up form on home page
    Added sign in form on home page
    Added change password form and sign out button wrapped in a hidden <div>
    Added UI functions for:
    -sign up success
    -sign up failure
    -sign in success
    -sign in failure
    -change password success
    -change password failure
    -sign out success
    
    Added event handlers for sign up, sign in, change password, and sign out

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const store = require('../store')
+
+const signUpSuccess = function () {
+  $('#sign-message').removeClass()
+  $('#sign-message').text('Successfully signed up')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+  setTimeout(() => {
+    $('#sign-message').html('')
+    $('#sign-message').addClass('hidden')
+  }, 3000
+  )
+}
+
+const signUpFailure = function () {
+  $('#sign-message').removeClass()
+  $('#signUpModal').modal('hide')
+  $('#sign-message').text('Failed to sign up')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+  setTimeout(() => {
+    $('#sign-message').html('')
+    $('#sign-message').addClass('hidden')
+  }, 3000
+  )
+}
+
+const signInSuccess = function (data) {
+  $('#sign-message').removeClass()
+  $('#sign-message').text('Successfully signed in')
+  $('#sign-forms').addClass('hidden')
+  $('#pwAndSignOut').removeClass()
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+  setTimeout(() => {
+    $('#sign-message').html('')
+    $('#sign-message').addClass('hidden')
+  }, 3000
+  )
+  store.user = data.user
+}
+
+const signInFailure = function () {
+  $('#sign-message').removeClass()
+  $('#sign-message').text('Failed to sign in')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+  setTimeout(() => {
+    $('#sign-message').addClass('hidden')
+    $('#sign-message').text('')
+  }, 3000
+  )
+}
+
+const changePasswordSuccess = function () {
+  $('#sign-message').removeClass()
+  $('#sign-message').text('Successfully changed password')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+  setTimeout(() => {
+    $('#sign-message').addClass('hidden')
+    $('#sign-message').text('')
+  }, 3000
+  )
+}
+
+const changePasswordFailure = function () {
+  $('#sign-message').removeClass()
+  $('#sign-message').text('Failed to change password')
+  $('input[type=text]').val('')
+  $('input[type=password]').val('')
+  setTimeout(() => {
+    $('#sign-message').addClass('hidden')
+    $('#sign-message').text('')
+  }, 3000
+  )
+}
+
+const signOutSuccess = function () {
+  $('#sign-message').removeClass()
+  $('#sign-message').text('Successfully signed out')
+  $('#sign-forms').removeClass()
+  $('#pwAndSignOut').addClass('hidden')
+  store.user = null
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess
+}
